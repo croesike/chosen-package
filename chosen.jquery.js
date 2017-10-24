acc8816e14eb71115989bc5388d369f944250ffb
Christian Rösike <chr@bytepark.de>
2017-10-24 17:21:41 +0200

    removed mobile check

--- chosen.jquery.js
+++ chosen.jquery.js
@@ -588,7 +588,7 @@
         return document.documentMode >= 8;
       }
       if (/iP(od|hone)/i.test(window.navigator.userAgent) || /IEMobile/i.test(window.navigator.userAgent) || /Windows Phone/i.test(window.navigator.userAgent) || /BlackBerry/i.test(window.navigator.userAgent) || /BB10/i.test(window.navigator.userAgent) || /Android.*Mobile/i.test(window.navigator.userAgent)) {
-        return false;
+        return true;
       }
       return true;
     };
